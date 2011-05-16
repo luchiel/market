3c9c2d5e97cb933f0559772cf443085ee2ea40fa
luchiel <luchiella@yandex.ru>
2011-05-16 17:32:55 +1100

    1. layout for new_comment 2. map: list of offices. Click to see one. 3. Map is centered to home, or to first office, or to V-City

--- theMarket/media/js/marketJSmap.js
+++ theMarket/media/js/marketJSmap.js
@@ -4,50 +4,55 @@
     map.enableScrollZoom();
     map.enableRightButtonMagnifier();
 
+    default_address = new YMaps.Geocoder('г. Владивосток', { results: 1 });
+    YMaps.Events.observe(default_address, default_address.Events.Load, function(default_address) {
+        map.setCenter(default_address.get(0).getBounds().getCenter(), 8);
+    });
+    var list = $('<ol>');
+    
     function addPlacemark(a, name, value, style) {
         var i = new YMaps.Placemark(a.get(0).getGeoPoint(), { style: style });
         i.name = name;
         i.description = value;
         map.addOverlay(i);
+        
+        var li = $('<li>');
+        li
+            .html(i.name + ': ' + i.description)
+            .click({ point: i }, function(eventObject) {
+                map.setCenter(eventObject.data.point.getGeoPoint(), 8);
+            })
+            .appendTo(list);
     }
     
-    //get addresses
     $.get(
         '/theMarket/map_data/', {},
         function(data) {
-            //process addresses
+            var addressCount = 0;
             $.each(
                 data,
                 function(index, value) {
-                    if(value == '' && index == 0)
-                        value = 'г. Владивосток';
-                    else if(value == '')
+                    if(value == '')
                         return;
+                        
                     var address = new YMaps.Geocoder(value, { results: 1 });
                     
                     YMaps.Events.observe(address, address.Events.Load, function(address) {
-                        if(index == 0) {
-                            if(address.length()) {
+                        if(address.length()) {
+                            addressCount++;
+                            if(index == 0)
                                 addPlacemark(address, 'Your location', value, 'default#houseIcon');
-                                map.setCenter(address.get(0).getBounds().getCenter(), 8);
-                            }
-                            else {
-                                default_address = new YMaps.Geocoder('г. Владивосток', { results: 1 });
-                                YMaps.Events.observe(default_address, default_address.Events.Load, function(default_address) {
-                                    addPlacemark(default_address, 'Your location', value, 'default#houseIcon');
-                                    map.setCenter(default_address.get(0).getBounds().getCenter(), 8);
-                                });
-                            }
-                        }
-                        else {
-                            if(address.length())
+                            else
                                 addPlacemark(address, 'Office', value, 'default#shopIcon');
+                            if(addressCount == 1 || index == 0)
+                                map.setCenter(address.get(0).getBounds().getCenter(), 8);
                         }
                     });
                 }
             );
         }
     );
+    $('#office_addresses').html('').append(list);
 }
 
 $(document).ready(function () {
